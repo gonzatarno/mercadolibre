9fc4109baa41aebe5d1e4decc9c0f41058d2ac55
Gonzalo Tarnofsky <gonzalotarnofsky@gmail.com>
2021-04-29 01:22:32 -0300

    Defining Shop cart and correct cde lines

--- js/app.js
+++ js/app.js
@@ -11,41 +11,32 @@ let carrito = []
         carrito = JSON.parse(ValorDelCarritoEnElStorage);
     }
 
-/*
-const Productos = [
-    {nombre: 'HyperX Cloud Flight', precio: 14799,imagen: 'hyperx.jpg',stock: 4,oferta: 'OFERTA DEL DÍA', categoria: 'consola' },
-    {nombre:'Joystick inalámbrico PlayStation',precio: 10000,imagen: 'joystick_ps5.jpg',stock: 0,oferta: 'OFERTA DEL DÍA', categoria: 'consola' },
-    {nombre:'Smart TV Tedge Led 50',precio: 25000,imagen: 'tedge.jpg',stock: 3,oferta: 'OFERTA DEL DÍA', categoria: 'televisor' },
-    {nombre:'Smart TV Samsung',precio: 31900,imagen: 'samsung.jpg',stock: 1,oferta: 'OFERTA DEL DÍA', categoria: 'televisor' },
-    {nombre:'Mouse Logitech G305',precio: 4000,imagen: 'mouse_g305.jpg',stock: 0,oferta: 'OFERTA DEL DÍA', categoria: 'computacion' },
-    {nombre:'Tablet Samsung',precio: 16100,imagen: 'tab_a.jpg',stock: 5,oferta: 'OFERTA DEL DÍA', categoria: 'computacion' }
-]
 
-*/
 
-//FUNCION CLASE DE PRODCTOS
-
-class Producto {
-    constructor(nombre, precio, imagen, stock, oferta, tipo) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-        this.stock = stock;
-        this.oferta = oferta;
-        this.tipo = tipo
-
-        const quitarStock = (cantidad) =>{
-            this.stock -= cantidad
+    class Producto {
+        constructor(nombre, precio, imagen, stock, oferta, tipo) {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.imagen = imagen;
+            this.stock = stock;
+            this.oferta = oferta;
+            this.tipo = tipo
+    
+            const quitarStock = (cantidad) =>{
+                this.stock -= cantidad
+            }
         }
     }
-}
+    
+    Productos.push(new Producto('HyperX Cloud Flight', 14799, 'hyperx.jpg', 4, 'OFERTA DEL DÍA', 'Consola'))
+    Productos.push(new Producto('Joystick inalámbrico PlayStation', 10000, 'joystick_ps5.jpg', 1, 'OFERTA DEL DÍA', 'Consola'))
+    Productos.push(new Producto('Smart TV Tedge Led 50', 25000, 'tedge.jpg', 3, 'OFERTA DEL DÍA', 'Televisor'))
+    Productos.push(new Producto('Smart TV Samsung', 31900, 'samsung.jpg', 1, 'OFERTA DEL DÍA', 'Televisor'))
+    Productos.push(new Producto('Mouse Logitech G305', 4000, 'mouse_g305.jpg', 3, 'OFERTA DEL DÍA', 'Computacion'))
+    Productos.push(new Producto('Tablet Samsung', 16100, 'tab_a.jpg', 5, 'OFERTA DEL DÍA', 'Computacion'))
+    
 
-Productos.push(new Producto('HyperX Cloud Flight', 14799, 'hyperx.jpg', 4, 'OFERTA DEL DÍA', 'Consola'))
-Productos.push(new Producto('Joystick inalámbrico PlayStation', 10000, 'joystick_ps5.jpg', 1, 'OFERTA DEL DÍA', 'Consola'))
-Productos.push(new Producto('Smart TV Tedge Led 50', 25000, 'tedge.jpg', 3, 'OFERTA DEL DÍA', 'Televisor'))
-Productos.push(new Producto('Smart TV Samsung', 31900, 'samsung.jpg', 1, 'OFERTA DEL DÍA', 'Televisor'))
-Productos.push(new Producto('Mouse Logitech G305', 4000, 'mouse_g305.jpg', 3, 'OFERTA DEL DÍA', 'Computacion'))
-Productos.push(new Producto('Tablet Samsung', 16100, 'tab_a.jpg', 5, 'OFERTA DEL DÍA', 'Computacion'))
+
 
 
 
@@ -121,7 +112,8 @@ Productos.sort((a, b) => b.precio - a.precio);
 //CARDS PRODUCTOS
 for (let i = 0; i < Productos.length; i++) {
     
-    acumulador += `<div class="col-lg-4 col-md-6 mb-4">
+    acumulador += `
+    <div class="col-lg-4 col-md-6 mb-4">
         <div class="card h-100 card-ml">
             <a href="#"><img class="card-img-top" src="./images/${Productos[i].imagen}" alt=""></a>
             <div class="card-body">
@@ -146,3 +138,10 @@ for (let i = 0; i < Productos.length; i++) {
 }
 
 document.getElementById("productos").innerHTML = acumulador;
+
+
+
+
+// Cosas a agregar:
+// Cantidad de productos en el carrito
+// filtro por boton de productos
---
+++ js/stock.js
@@ -0,0 +1,34 @@
+/*
+const Productos = [
+    { nombre: 'HyperX Cloud Flight', precio: 14799 , imagen: 'hyperx.jpg' , stock: 4 ,oferta: 'OFERTA DEL DÍA' , categoria: 'consola' },
+    { nombre:'Joystick inalámbrico PlayStation', precio: 10000 , imagen: 'joystick_ps5.jpg', stock: 0 , oferta: 'OFERTA DEL DÍA', categoria: 'consola' },
+    { nombre:'Smart TV Tedge Led 50',precio: 25000 , imagen: 'tedge.jpg' ,stock: 3  , oferta: 'OFERTA DEL DÍA' , categoria: 'televisor' },
+    { nombre:'Smart TV Samsung' ,precio: 31900 , imagen: 'samsung.jpg', stock: 1 , oferta: 'OFERTA DEL DÍA', categoria: 'televisor' },
+    { nombre:'Mouse Logitech G305', precio: 4000 , imagen: 'mouse_g305.jpg', stock: 0 , oferta: 'OFERTA DEL DÍA', categoria: 'computacion' },
+    { nombre:'Tablet Samsung', precio: 16100 , imagen: 'tab_a.jpg', stock: 5 , oferta: 'OFERTA DEL DÍA', categoria: 'computacion' }
+]
+
+
+//FUNCION CLASE DE PRODCTOS
+class Producto {
+    constructor(nombre, precio, imagen, stock, oferta, tipo) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.stock = stock;
+        this.oferta = oferta;
+        this.tipo = tipo
+
+        const quitarStock = (cantidad) =>{
+            this.stock -= cantidad
+        }
+    }
+}
+
+Productos.push(new Producto('HyperX Cloud Flight', 14799, 'hyperx.jpg', 4, 'OFERTA DEL DÍA', 'Consola'))
+Productos.push(new Producto('Joystick inalámbrico PlayStation', 10000, 'joystick_ps5.jpg', 1, 'OFERTA DEL DÍA', 'Consola'))
+Productos.push(new Producto('Smart TV Tedge Led 50', 25000, 'tedge.jpg', 3, 'OFERTA DEL DÍA', 'Televisor'))
+Productos.push(new Producto('Smart TV Samsung', 31900, 'samsung.jpg', 1, 'OFERTA DEL DÍA', 'Televisor'))
+Productos.push(new Producto('Mouse Logitech G305', 4000, 'mouse_g305.jpg', 3, 'OFERTA DEL DÍA', 'Computacion'))
+Productos.push(new Producto('Tablet Samsung', 16100, 'tab_a.jpg', 5, 'OFERTA DEL DÍA', 'Computacion'))
+*/
