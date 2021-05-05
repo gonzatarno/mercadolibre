d60a55389e095c8deb6ccd9b608de298d1a2db46
Gonzalo Tarnofsky <gonzalotarnofsky@gmail.com>
2021-05-05 12:39:13 -0300

    Adding productos quantity in cart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-let acumulador = ``;
 let Productos=[]
+const contenedorProductos = document.getElementById('productos')
+const contadorCarrito = document.getElementById('numero-carrito')
+const contenedorCarrito = document.getElementById('carrito')
+const precioTotal = document.getElementById('total-carrito')
 
 
 let ValorDelCarritoEnElStorage = localStorage.carrito; 
@@ -34,55 +37,78 @@ const usuarioNombre = document.getElementById('usuario-nombre')
 
 
 //CARDS PRODUCTOS
-mostrarProductos(Productos)
-function mostrarProductos() {
-    for (let i = 0; i < stockProductos.length; i++) {
-    
-        acumulador += `
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100 card-ml">
-                <a href="#"><img class="card-img-top" src="./images/${stockProductos[i].imagen}" alt=""></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                    <a href="#">${stockProductos[i].nombre}</a>
-                    </h4>
-                    <h6 class="badge bg-primary" id="ofertaDelDia">OFERTA DEL DÍA</h6>
-                    <h5 class="precio-card"> $${stockProductos[i].precio}</h5>
-                    <p class="texto-envio">Envío gratis</p>
-                </div>
-                <div class="container-truck">
-                    <div class="circle-truck">
-                        <img class="truck" src="/images/truck_2.png" width="20px" height="20px" alt="truck">
-                    </div>
-                </div>
-                <div class="card-footer-meli">
-                    <button onclick='agregarCarrito(${JSON.stringify(stockProductos[i])})' class="btn btn-primary btn-meli">Agregar al carrito</button>
-                </div>
-            </div>
-        </div>`;
-        
-    }
+    mostrarProductos(stockProductos)
 
-document.getElementById( "productos").innerHTML = acumulador;
-}
+    function mostrarProductos(array) {
+
+        contenedorProductos.innerHTML = ''
+
+        array.forEach( (producto) => {
+            const div = document.createElement('div')
+            div.classList.add('producto')
+            div.innerHTML = `
+                <div class="card h-100 card-ml">
+                    <a href="#"><img class="card-img-top" src="./images/${producto.imagen}" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                        <a href="#">${producto.nombre}</a>
+                        </h4>
+                        <h6 class="badge bg-primary" id="ofertaDelDia">OFERTA DEL DÍA</h6>
+                        <h5 class="precio-card"> $${producto.precio}</h5>
+                        <p class="texto-envio">Envío gratis</p>
+                    </div>
+                    <div class="container-truck">
+                        <div class="circle-truck">
+                            <img class="truck" src="/images/truck_2.png" width="20px" height="20px" alt="truck">
+                        </div>
+                    </div>
+                    <div class="card-footer-meli">
+                        <button onclick='agregarAlCarrito(${producto.id})' class="btn btn-primary btn-meli">Agregar al carrito</button>
+                    </div>
+                </div>`
+            
+            
+            contenedorProductos.appendChild(div)
+        } )
+    }
 
 
 
 //Agregar al Carrito
-function agregarCarrito(objetoCarrito) {
+function agregarAlCarrito(itemId) {
 
-    carrito.push(objetoCarrito)
-    cargarCarrito()
+    let itemEnCarrito = carrito.find( el => el.id == itemId )
+
+    if (itemEnCarrito) {
+        itemEnCarrito.cantidad += 1
+        contadorCarrito.innerText = carrito.length += 1
+    } else {
+        let {id, nombre, precio, imagen} = stockProductos.find( el => el.id == itemId )
+        carrito.push({id: id, nombre: nombre, precio: precio, cantidad: 1, imagen: imagen})
+    }
+
     localStorage.setItem('carrito', JSON.stringify(carrito))
-    
+
+    console.log(carrito)
+
+    contadorCarrito.innerText = carrito.length
+    actualizarCarrito()
 }
 
 //Quitar del carrito
-function quitarDelCarrito(indice) {
+function quitarDelCarrito(id) {
 
-    carrito.splice(indice, 1)
-    cargarCarrito()
+    let productoAEliminar = carrito.find( el => el.id == id )
 
+    productoAEliminar.cantidad--
+
+    if (productoAEliminar.cantidad == 0) {
+        let indice = carrito.indexOf(productoAEliminar)
+        carrito.splice(indice, 1)
+    }
+
+    console.log(carrito)
+    actualizarCarrito()
 }
 
 
@@ -90,47 +116,50 @@ function quitarDelCarrito(indice) {
 function vaciarCarrito(){
     localStorage.clear()
     carrito = []
-    numeroCarrito = ""
-    totalCarrito = 0
-    document.getElementById ('carrito').innerHTML = carrito;
-    document.getElementById('numero-carrito').innerHTML = numeroCarrito
-    document.getElementById('total-carrito').innerHTML = `Total: $${totalCarrito}`
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = 'Total: $'+ carrito.reduce( (acc, el) => acc += el.precio, 0 ) 
+    
+    actualizarCarrito()
 }
 
 
 
 //Actualizar el carrito
-function cargarCarrito(){
-    let productosEnCarrito = ''
-    let numeroCarrito = 0
-    let totalCarrito = 0
-
-    for (let i = 0; i < carrito.length; i++) {
-        productosEnCarrito += `<div class="contenedor-carrito-producto-div">
-        <div class="contenedor-carrito-producto">
-            <img src="/images/${carrito[i].imagen}" alt="">
-            <div class="contenedor-carrito-prducto-texto">
-                <h4>${carrito[i].nombre}</h4>
-            <p class="texto-envio">Envío gratis</p>
+function actualizarCarrito(){
+    contenedorCarrito.innerHTML=''
+    
+        carrito.forEach( (producto) => {
+    
+            const div = document.createElement('div')
+            div.classList.add('productoEnCarrito')
+            div.innerHTML = `
+            <div class="contenedor-carrito-producto-div">
+                <div class="contenedor-carrito-producto">
+                    <img src="/images/${producto.imagen}" alt="">
+                    <div class="contenedor-carrito-prducto-texto">
+                        <h4>${producto.nombre}</h4>
+                    <p class="texto-envio">Envío gratis</p>         
+                    </div>
+                        <div class="precio-cantidad">
+                             <h2>$${producto.precio * producto.cantidad}</h2>
+                            <p class="cantidad">${producto.cantidad}<p>
+                        </div>              
+                </div>
             </div>
-            <h2>$${carrito[i].precio}</h2>
-        </div>
-    </div>
-    <div class="btn-eliminar-grupo">
-    <div class="btn-eliminar" onclick="quitarDelCarrito(${[i]})">Eliminar</div>
-    <div class="btn-eliminar" onclick="vaciarCarrito(${[i]})">Vaciar carrito</div>
-    </div>
-    <div class="contenedor-carrito-linea"></div></div>`
+            <div class="btn-eliminar-grupo">
+                <div class="btn-eliminar" onclick="quitarDelCarrito(${producto.id})">Eliminar</div>
+                <div class="btn-eliminar" onclick="vaciarCarrito(${producto.id})">Vaciar carrito</div>
+            </div>
+            <div class="contenedor-carrito-linea"></div></div>`
+    
+            contenedorCarrito.appendChild(div)        
+        })
 
-    totalCarrito += carrito[i].precio + carrito[i].precio * 0.21
-    numeroCarrito += 1
-        
+        //NUMERO CARRITO
+        contadorCarrito.innerText = carrito.length
+        precioTotal.innerText = 'Total: $'+ carrito.reduce( (acc, el) => acc += (el.precio * el.cantidad), 0 )
     }
 
-    document.getElementById('carrito').innerHTML = productosEnCarrito
-    document.getElementById('numero-carrito').innerHTML = numeroCarrito
-    document.getElementById('total-carrito').innerHTML = `Total: $${totalCarrito}`
-}
 
 
 //Al clicker borrar o vaciar carrito, sigue en la misma pantalla
@@ -142,9 +171,7 @@ cajaCarrito.addEventListener('click', (event)=>{
 
 
 
-// filtro por boton de productos
-
-
 
 // Cosas a agregar:
 // Cantidad de productos en el carrito
+// filtro por boton de productos
